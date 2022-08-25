7f20f7270a3f1300b2f5de04e7bd7e0564e5d869
Oscar Garcia <oscar.garcia@theksquaregroup.com>
2022-08-24 19:21:26 -0500

    Remove violation cause error

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -6,4 +6,5 @@ echo "Install SFDX Scanner"
 echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
 
 echo "Running SFDX Scanner"
-npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv" --violations-cause-error
+#npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv" --violations-cause-error
+npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv"
